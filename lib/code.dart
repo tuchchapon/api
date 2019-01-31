5ba689e54bdf39e279c4e891e20265b7a46763c1
tuchchapon <35054396+tuchchapon@users.noreply.github.com>
2019-01-31 11:41:56 +0700

    test1

--- lib/code.dart
+++ lib/code.dart
@@ -1,19 +1,24 @@
-import 'dart:async';
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:async';
 import 'package:http/http.dart' as http;
 
 Future<Post> fetchPost() async {
-  final response = await http.get('http://35.198.219.154:1337/position');
-
-  if (response.statusCode == 200) {
-    // If the call to the server was successful, parse the JSON
+  final response = await http.get('http://35.198.219.154:1337/position/1');
+  if (response.statusCode == 200)
     return Post.fromJson(json.decode(response.body));
-  } else {
-    // If that call was not successful, throw an error.
-    throw Exception('Failed to load post');
-  }
+  else
+    throw Exception('fail');
+}
+
+List<Post> postFromJson(String str) {
+  final jsonData = json.decode(str);
+  return new List<Post>.from(jsonData.map((x) => Post.fromJson(x)));
+}
+
+String postToJson(List<Post> data) {
+  final dyn = new List<dynamic>.from(data.map((x) => x.toJson()));
+  return json.encode(dyn);
 }
 
 
@@ -49,17 +54,65 @@ class Post {
   };
 }
 
-class branch extends StatefulWidget {
-  @override
-  _branchState createState() => _branchState();
+
+void main() {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
 }
 
-class _branchState extends State<branch> {
+class MyApp extends StatefulWidget {
   @override
+  _State createState() => new _State();
+}
+
+class _State extends State<MyApp> {
+  @override
+
   Widget build(BuildContext context) {
-    return Container(
+    MediaQueryData queryData = MediaQuery.of(context);
 
+    double screenWidth = queryData.size.width;
+    double screenHeight = queryData.size.height;
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Name here'),
+      ),
+      body: new Container(height: screenHeight,width: screenWidth,margin: EdgeInsets.all(5),
+        padding: new EdgeInsets.all(32.0),
+        child: new ListView(
+          children: <Widget>[
+            RaisedButton(onPressed: null,child: Text('sadsdf'),),
+            Card(child:
+            FutureBuilder<Post>(
+              future: fetchPost(),
+              builder: (context,snapshot){
+                if (snapshot.hasData){
+                  String create = snapshot.data.createdAt.toString();
+                  String update = snapshot.data.updatedAt.toString();
+                  String ID = snapshot.data.id.toString();
+                  String Position = snapshot.data.positionName;
+                  String Status = snapshot.data.status.toString();
+                  return Column(mainAxisAlignment: MainAxisAlignment.center,children: <Widget>[
+                    Text('create :$create'),
+                    Text('update :$update'),
+                    Text('ID : $ID'),
+                    Text('Position :$Position'),
+                    Text('Status :$Status'),
+
+
+                  ],);
+                }
+                else if (snapshot.hasError)
+                  return Text(snapshot.error);
+              },
+
+            ),
+
+            )
+          ],
+        ),
+      ),
     );
-
   }
 }
